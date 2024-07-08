3e96aaf99b5c6be973e6621a17af4dbee733a482
Reryson Farinha <rerysonfarinha@outlook.com>
2024-07-08 12:00:35 -0300

    Criando arquivo Json a partir do CEP

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
 
         BuscadorDeCep consultaCep = new BuscadorDeCep();
 
-        Endereco novoEndereco = consultaCep.buscarCep("01001000");
-        System.out.println(novoEndereco);
+        try {
+            Endereco novoEndereco = consultaCep.buscarCep("01001000");
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+
+        }catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando Aplicação...");
+        }
     }
 }
